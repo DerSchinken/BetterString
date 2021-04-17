f4aea6c924ed5e891d60cec230caf2c83a3bafd9
DrBumm <53398996+DrBumm@users.noreply.github.com>
2021-04-17 14:29:37 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     name="BetterString",
     version="2.7.6",
     # Major version 2
-    # Minor version 5
-    # Maintenance version 5
+    # Minor version 7
+    # Maintenance version 6
 
     author="DerSchinken (aka DrBumm)",
     description="Like a normal string but with more functionality",
